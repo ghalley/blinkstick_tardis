33d0d19f0d1ae3d89b260f97915a546348fa3fe9
Gabriel Halley <gabhalley@gmail.com>
2017-09-02 14:18:25 -0500

    move the lights faster

--- square.py
+++ square.py
@@ -3,11 +3,11 @@ import time
 
 def loop_square():
   bstick = blinkstick.find_first()
-  for y in range(0,3):
+  for y in range(0,10):
     for x in range(0,8):
       if x>=1: bstick.set_color(channel=0, index=(x-1))
       if x<8:  bstick.set_color(channel=0, index=x, name="blue")
-      time.sleep(0.25)
+      time.sleep(0.05)
     turn_off_square()
 
 def turn_off_square():
