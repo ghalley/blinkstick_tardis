96a085ffbc801587af6e21b15d70043b570fb928
Gabriel Halley <gabhalley@gmail.com>
2017-09-02 14:00:40 -0500

    move one light around the square

---
+++ square.py
@@ -0,0 +1,21 @@
+from blinkstick import blinkstick
+import time
+
+def loop_square():
+  bstick = blinkstick.find_first()
+  for y in range(0,3):
+    for x in range(0,8):
+      if x>=1: bstick.set_color(channel=0, index=(x-1))
+      if x<8:  bstick.set_color(channel=0, index=x, name="blue")
+      time.sleep(0.25)
+    turn_off_square()
+
+def turn_off_square():
+  bstick = blinkstick.find_first()
+  for x in range(0,8):
+    bstick.set_color(channel=0, index=x)
+
+
+
+loop_square()
+
